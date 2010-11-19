c76f767532c745c2a90fcb4f4ad1fe5155a5a101
vandonr <vandonr@3bedcf18-2fa8-1abf-5171-490318676d27>
2010-11-19 17:13:36 +0000

    création des modèles

--- cinemac/apps/movies/models.py
+++ cinemac/apps/movies/models.py
@@ -1,3 +1,113 @@
 from django.db import models
 
-# models here.
+class Genre(models.Model):
+	slug 		= models.SlugField(max_length = 255)
+	name 		= models.CharField(max_length = 255)
+	
+	def __unicode__(self):
+		return "%u" % (self.name)
+		
+class Artist(models.Model):
+	slug 		= models.SlugField(max_length = 255)
+	name 		= models.CharField(max_length = 255)
+	forename	= models.CharField(max_length = 255)
+	
+	def __unicode__(self):
+		return "%u %u" % (self.forename, self.name)
+		
+class Course(models.Model):
+	slug 		= models.SlugField(max_length = 255)
+	year		= models.PositiveSmallIntegerField()
+	topic		= models.CharField(max_length = 255)
+	teacher		= models.CharField(max_length = 255)
+	
+	def __unicode__(self):
+		return "%u" % (self.topic)
+		
+class CourseComment(models.Model):
+	slug 		= models.SlugField(max_length = 255)
+	date		= models.DateTimeField(auto_now_add=True)
+	comment		= models.TextField()
+	
+	course		= models.ForeignKey('Course')
+	movie		= models.ForeignKey('Movie')
+	author		= models.ForeignKey('Member')
+	
+	def __unicode__(self):
+		return "%u" % (self.comment)
+		
+class Movie(models.Model):
+	id_imdb		= models.PositiveInteger()
+	slug 		= models.SlugField(max_length = 255)
+	title 		= models.CharField(max_length = 255)
+	cover		= models.ImageField(upload_to='media/img/')
+	year 		= models.DateField()
+	country		= models.CharField(max_length = 255)
+	synopsis	= models.TextField()
+	
+	genre_is	= models.ManyToManyField('Genre')
+	played_by	= models.ManyToManyField('Artist')
+	directed_by	= models.ManyToManyField('Artist')
+	
+	def __unicode__(self):
+		return "%u (%u)" % (self.title, self.year.year)
+		
+
+class Rate(models.Model):
+	slug 		= models.SlugField(max_length = 255)
+	value		= models.PositiveSmallIntegerField()
+	comment		= models.TextField()
+	
+	movie		= ForeignKey('Movie')
+	author		= ForeignKey('Member')
+	
+	def __unicode__(self):
+		return "%u" % (self.value)
+		
+
+class CommentMovie(models.Model):
+	slug 		= models.SlugField(max_length = 255)
+	date		= models.DateTimeField(auto_now_add=True)
+	content		= models.TextField()
+	
+	movie		= models.ForeignKey('Movie')
+	author		= models.ForeignKey('Member')
+	
+	def __unicode__(self):
+		return "%u" % (self.comment)
+		
+class CommentEvent(models.Model):
+	slug 		= models.SlugField(max_length = 255)
+	date		= models.DateTimeField(auto_now_add=True)
+	content		= models.TextField()
+	
+	movie		= models.ForeignKey('Event')
+	author		= models.ForeignKey('Member')
+	
+	def __unicode__(self):
+		return "%u" % (self.comment)
+		
+class Event(models.Model):
+	slug 		= models.SlugField(max_length = 255)
+	date		= models.DateTimeField()
+	location	= models.CharField(max_length = 255)
+	description	= models.TextField()
+	
+	movie		= models.ForeignKey('Movie')
+	creator		= models.ForeignKey('Member')
+	
+	def __unicode__(self):
+		return "%u, %u" % (self.location, self.date)
+		
+class Member(models.Model):
+	slug 		= models.SlugField(max_length = 255)
+	pseudo		= models.CharField(max_length = 255)
+	mail		= models.EmailField();
+	password	= models.CharField(max_length = 255);
+	date_joined	= models.DateTimeField(auto_now_add=True)
+	class_year	= models.DateField();
+	avatar		= models.ImageField(upload_to='media/img/avatars/')
+	
+	def __unicode__(self):
+		return "%u" % (self.pseudo)
+		
