252892544c61fbcb1b5181be9e08dc0ab2364667
quentin.dangles@laposte.net <quentin.dangles@laposte.net@3bedcf18-2fa8-1abf-5171-490318676d27>
2010-11-25 11:33:40 +0000

    créé un nouveau formulaire, et donc modifié url et views en fonction

--- trunk/cinemac/apps/movies/urls.py
+++ trunk/cinemac/apps/movies/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
 	url(r'^$', index, name='index'),
 	url(r'^films/$', fichefilm, name='fichefilm'),
+    url(r'^profil/$', profil, name='profil'),
 )
