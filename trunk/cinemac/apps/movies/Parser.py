b2c121afad9a36530b58678ea596d07ad56ed716
vandonr <vandonr@3bedcf18-2fa8-1abf-5171-490318676d27>
2010-11-30 14:06:55 +0000

    parser ok et bdd modifiée

--- trunk/cinemac/apps/movies/Parser.py
+++ trunk/cinemac/apps/movies/Parser.py
@@ -16,14 +16,14 @@ class MovieParser:
 	
 	#fields from imdb
 	id_imdb = 0
-	genres = list()
 	rating = 0.0
+	genres = list()
 	
 	#other fields
 	#slug
 	#cover
 	
-	def get_cast_by_activity(casting, activity_code):
+	def get_cast_by_activity(self, casting, activity_code):
 		people = list()
 		for e in casting:
 		    if int(e.getElementsByTagName("activity")[0].attributes["code"].value) == activity_code:
@@ -34,13 +34,13 @@ class MovieParser:
 		f = xml.dom.minidom.parse(xmlfile)
 		e = f.getElementsByTagName("movie")[0] #the global element containing everyting
 		self.title		= e.getElementsByTagName("title")[0].childNodes[0].nodeValue
-		self.year		= int(e.getElementsByTagName("productionYear")[0].childNodes[0].nodeValue)
-		countrylist	= e.getElementsByTagName("nationalityList")[0]
+		self.year		= e.getElementsByTagName("productionYear")[0].childNodes[0].nodeValue
+		countrylist		= e.getElementsByTagName("nationalityList")[0]
 		self.country		= countrylist.getElementsByTagName("nationality")[0].childNodes[0].nodeValue
 		self.synopsis	= e.getElementsByTagName("synopsis")[0].childNodes[0].nodeValue
-		casting		= e.getElementsByTagName("casting")[0].getElementsByTagName("castMember")
-		self.directors	= get_cast_by_activity(casting, REA_CODE)
-		self.actors		= get_cast_by_activity(casting, ACT_CODE)
+		casting			= e.getElementsByTagName("casting")[0].getElementsByTagName("castMember")
+		self.directors	= self.get_cast_by_activity(casting, REA_CODE)
+		self.actors		= self.get_cast_by_activity(casting, ACT_CODE)
 		
 	def parse_imdb(self, imdbfile):
 		fp = open(imdbfile, 'r')
@@ -49,12 +49,14 @@ class MovieParser:
 		self.genres = f['genres'].split(',')
 		self.id_imdb = int(f["imdburl"].rsplit("/", 2)[1].lstrip("tt")) #last non-null field of the url separated by /, stripped from its leading "tt" and converted to int, is the imdb ID.
 		
-	def insert():
+	def insert(self):
 		mov = Movie()
-		mov.title = title;
-		mov.year = year;
-		mov.country = country;
-		mov.synopsis = synopsis;
+		mov.title = self.title
+		mov.year = self.year + "-01-01"
+		mov.country = self.country
+		mov.synopsis = self.synopsis
+		
+		mov.id_imdb = self.id_imdb
 		#...
 		mov.save() #ecrit dans la BDD
 		
