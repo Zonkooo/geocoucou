f8f5b9d3f8c09da605d7eff23c5eac801909d69c
quentin.dangles@laposte.net <quentin.dangles@laposte.net@3bedcf18-2fa8-1abf-5171-490318676d27>
2010-11-30 11:08:16 +0000

    avec le formulaire favourite film qui marche

--- cinemac/apps/movies/views.py
+++ cinemac/apps/movies/views.py
@@ -6,12 +6,22 @@ from apps.movies.forms import *
 def index(request):
 	return render_to_response('cinemac/index.html')
 
-	
-def fichefilm(request):
-	return render_to_response('cinemac/fichefilm.html')
-
 def profil(request):
-        form = ProfilForm()
+        if request.method == 'POST':
+                form = ProfilForm(request.POST)
+                if form.is_valid():
+					favouriteFilm = form.cleaned_data['favouriteFilm']
+					#Creation dun objet du type de la table voulue
+					#Passage par parametre TABLE_VOULUE(nom_attribut_table=.., nom_attribut_table=..)
+					monFilmFavori = Genre(name=favouriteFilm)
+					#Enregistrement dans la Base De Donnees avec monObjet.save()
+					monFilmFavori.save()
+					#return HttpResponseRedirect('#') # Redirect after POST
+        else:
+                form = ProfilForm()
+
         return render_to_response('cinemac/profil.html',{
                 'form':form,
         })
+        
+
