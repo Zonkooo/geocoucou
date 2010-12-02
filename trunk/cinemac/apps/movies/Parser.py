f6b71208a84a7c2a215e771f54aca5b74e82edff
vandonr <vandonr@3bedcf18-2fa8-1abf-5171-490318676d27>
2010-12-02 19:38:32 +0000

    yeah, the parser is finally fully working bitches ! (enfin, à peu près)

--- trunk/cinemac/apps/movies/Parser.py
+++ trunk/cinemac/apps/movies/Parser.py
@@ -1,6 +1,7 @@
 import xml.dom.minidom #for allocine
 import json #for imdb
-from models import Movie
+import urllib2
+from models import Movie, Genre, Artist
 
 REA_CODE = 8002
 ACT_CODE = 8001
@@ -24,15 +25,17 @@ class MovieParser:
 	#slug
 	#cover
 	
-	def get_cast_by_activity(self, casting, activity_code):
-		people = list()
-		for e in casting:
-		    if int(e.getElementsByTagName("activity")[0].attributes["code"].value) == activity_code:
-				people.append(e.getElementsByTagName("person")[0].childNodes[0].nodeValue)
-		return people
+	def __init__(self, id_allocine):
+		url = "http://api.allocine.fr/xml/movie?partner=1&code=" + id_allocine
+		xml_content = urllib2.build_opener().open(url).read()
+		self.parse_allocine_xml(xml_content)
 		
-	def parse_allocine_xml(self, xmlfile):
-		f = xml.dom.minidom.parse(xmlfile)
+		url = "http://www.deanclatworthy.com/imdb/?q=" + self.title_en.replace(' ', '+')
+		imdb_content = urllib2.build_opener().open(url).read()
+		self.parse_imdb(imdb_content)
+		
+	def parse_allocine_xml(self, xml_content):
+		f = xml.dom.minidom.parseString(xml_content)
 		e = f.getElementsByTagName("movie")[0] #the global element containing everyting
 		self.title		= e.getElementsByTagName("title")[0].childNodes[0].nodeValue
 		self.title_en	= e.getElementsByTagName("originalTitle")[0].childNodes[0].nodeValue
@@ -44,12 +47,19 @@ class MovieParser:
 		self.directors	= self.get_cast_by_activity(casting, REA_CODE)
 		self.actors		= self.get_cast_by_activity(casting, ACT_CODE)
 		
-	def parse_imdb(self, imdbfile):
-		fp = open(imdbfile, 'r')
-		f = json.load(fp)
+	def get_cast_by_activity(self, casting, activity_code):
+		people = list()
+		for e in casting:
+		    if int(e.getElementsByTagName("activity")[0].attributes["code"].value) == activity_code:
+				people.append(e.getElementsByTagName("person")[0].childNodes[0].nodeValue)
+		return people
+		
+	def parse_imdb(self, imdbfile_content):
+		f = json.loads(imdbfile_content)
 		self.rating = float(f['rating'])
 		self.genres = f['genres'].split(',')
-		self.id_imdb = int(f["imdburl"].rsplit("/", 2)[1].lstrip("tt")) #last non-null field of the url separated by /, stripped from its leading "tt" and converted to int, is the imdb ID.
+		#last non-null field of the url separated by /, stripped from its leading "tt" and converted to int, is the imdb ID.
+		self.id_imdb = int(f["imdburl"].rsplit("/", 2)[1].lstrip("tt")) 
 		
 	def insert(self):
 		mov = Movie()
@@ -67,31 +77,42 @@ class MovieParser:
 		#many to many
 		#realisateurs
 		for a in self.directors:
-			a_forename = s.split(' ', 1)[0]
-			a_name = s.split(' ', 1)[1]
+			a_forename = a.split(' ', 1)[0]
+			a_name = a.split(' ', 1)[1]
 			
-			#checker les artistes déjà présents
+			#checker les artistes deja presents
 			qset = Artist.objects.filter(name = a_name, forename = a_forename)
-			if(qset.count() > 0)
+			if qset.count() > 0 :
 				mov.directed_by.add(qset[0])
-			else #creer ceux qui manquent
+			else : #creer ceux qui manquent
 				dr = Artist(name = a_name, forename = a_forename)
 				dr.save()
 				mov.directed_by.add(dr)
 		
-		#acteurs
+		#acteurs (un peu de copier coller bien laid)
 		for a in self.actors:
-			a_forename = s.split(' ', 1)[0]
-			a_name = s.split(' ', 1)[1]
+			a_forename = a.split(' ', 1)[0]
+			a_name = a.split(' ', 1)[1]
 			
-			#checker les artistes déjà présents
+			#checker les artistes deja presents
 			qset = Artist.objects.filter(name = a_name, forename = a_forename)
-			if(qset.count() > 0)
-				mov.directed_by.add(qset[0])
-			else #creer ceux qui manquent
+			if qset.count() > 0 :
+				mov.played_by.add(qset[0])
+			else : #creer ceux qui manquent
 				act = Artist(name = a_name, forename = a_forename)
 				act.save()
 				mov.played_by.add(act)
 				
-		mov.save() #on resauvegarde pour écrire les many_to_many
+		#et encore du copier coller
+		for g in self.genres:
+			#checker les genres deja presents
+			qset = Genre.objects.filter(name = g)
+			if qset.count() > 0 :
+				mov.genre_is.add(qset[0])
+			else : #creer ceux qui manquent
+				genre = Genre(name = g)
+				genre.save()
+				mov.genre_is.add(genre)
+		
+		mov.save() #on resauvegarde pour ecrire les many_to_many
 		
