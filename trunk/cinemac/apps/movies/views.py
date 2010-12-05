68b808c23a0cf372e2b1fa524f0117de58ada44c
vandonr <vandonr@3bedcf18-2fa8-1abf-5171-490318676d27>
2010-12-05 16:04:03 +0000

    fiche film : dodone (version sale)

--- trunk/cinemac/apps/movies/views.py
+++ trunk/cinemac/apps/movies/views.py
@@ -5,17 +5,44 @@ from apps.movies.forms import *
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
+from django.template import RequestContext
 
 def index(request):
 	return render_to_response('cinemac/index.html')
 
 def fichefilm(request):
-	return render_to_response('cinemac/fichefilm.html')
+	if request.method == 'GET':
+		movie_id = int(request.GET['mid'])
+		movie = Movie.objects.get(id = movie_id)
+		
+		#bricolage a corriger avec du code dans le html
+		dir_string = ""
+		act_string = ""
+		genre_string = ""
+		for d in movie.directed_by.all():
+			dir_string += d.__unicode__() + ", "
+		for a in movie.played_by.all():
+			act_string += a.__unicode__() + ", "
+		for g in movie.genre_is.all():
+			genre_string += g.__unicode__() + ", "
+		
+		val = {
+				"title"		: movie.title,
+				"directors"	: dir_string,
+				"actors"	: act_string,
+				"genres"	: genre_string,
+				"year"		: movie.year.year,
+				"country"	: movie.country,
+				"synopsis"	: movie.synopsis,
+			  }
+		return render_to_response('cinemac/fichefilm.html', val, context_instance = RequestContext(request) )
+	else:
+		return render_to_response('cinemac/fichefilm.html')
 
 def profil(request):
-    if request.method == 'POST':
-        form = ProfilForm(request.POST)
-        if form.is_valid():
+	if request.method == 'POST':
+		form = ProfilForm(request.POST)
+		if form.is_valid():
 			favouriteFilm = form.cleaned_data['favouriteFilm']
 			#Creation dun objet du type de la table voulue
 			#Passage par parametre TABLE_VOULUE(nom_attribut_table=.., nom_attribut_table=..)
@@ -23,8 +50,8 @@ def profil(request):
 			#Enregistrement dans la Base De Donnees avec monObjet.save()
 			monFilmFavori.save()
 			return HttpResponseRedirect('#') # Redirect after POST
-    else:
-        form = ProfilForm()
+	else:
+		form = ProfilForm()
 				
 	return render_to_response('cinemac/profil.html',{
 		'form':form,
@@ -48,19 +75,19 @@ def listeMembre(request):
 @csrf_exempt
 #todo : revoir la recherche et tester si elle fonctionne bien
 def resultatRecherche(request):
-    q = request.POST['content']
+	q = request.POST['content']
 	
-    clause = Q(slug__icontains=q)               
-    members = Member.objects.filter(clause).distinct()
-    artists = Artist.objects.filter(clause).distinct()
-    films = Movie.objects.filter(clause).distinct()
+	clause = Q(slug__icontains=q)			   
+	members = Member.objects.filter(clause).distinct()
+	artists = Artist.objects.filter(clause).distinct()
+	films = Movie.objects.filter(clause).distinct()
 		
-    return render_to_response('cinemac/resultatRecherche.html',{
+	return render_to_response('cinemac/resultatRecherche.html',{
 	'members_list' : members,
 	'artists_list' : artists,
 	'films_list' : films
 	})
 	
-        
+		
 
 Artist
