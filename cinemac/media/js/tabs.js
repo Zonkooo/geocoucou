d51ac44f85ecaae3528dd67e8a375582ea2d1c98
Cmellany91@gmail.com <Cmellany91@gmail.com@3bedcf18-2fa8-1abf-5171-490318676d27>
2010-12-06 09:24:11 +0000

    MAJ JS pour menu, vieweuse et tabs (mais tabs ne fonctionne pas)

--- cinemac/media/js/tabs.js
+++ cinemac/media/js/tabs.js
@@ -2,14 +2,8 @@
 {
 	alert('Start tabs');
 	// Tabs
-	alert('tabs');
 	$('#tabs').tabs();
-	alert('Modif tabs');
-	
-	//hover states on the static widgets
-	$('#dialog_link, ul#icons li').hover(
-		function() { $(this).addClass('ui-state-hover'); }, 
-		function() { $(this).removeClass('ui-state-hover'); }
-	);
+	alert('$(#tabs).tabs();');
+				
 				
 });
