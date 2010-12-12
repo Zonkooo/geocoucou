449cdaf58c3725ed848e174f641b6e85d7fe0967
vandonr <vandonr@3bedcf18-2fa8-1abf-5171-490318676d27>
2010-12-12 14:06:25 +0000

    normalement mon profil disparait quand on est pas loggué

--- trunk/cinemac/apps/movies/templatetags/logged_user.py
+++ trunk/cinemac/apps/movies/templatetags/logged_user.py
@@ -13,8 +13,27 @@ class CurrentUserNode(template.Node):
 			return_string = "Vous n'&ecirc;tes pas connect&eacute"
 			
 		return return_string
+		
+class AccountAccessNode(template.Node): #attention, code très sale
+	def render(self, context):
+		try : 
+			m = Member.objects.get(contrib_user = context['user'])
+			#on a réussi à choper l'user donc il est loggué
+			return """<li class="toggleSubMenu"><span>Mon compte</span> 
+						<ul class="subMenu"> 
+							<li><a href="/profil/" title="Voir mon profil">Voir mon profil</a></li> 
+							<li><a href="/login/" title="Modifier mon profil">Modifier mon profil</a></li> 				 
+							<li><a href="/listeMesInvit/" title="Mes invitations">Mes invitations</a></li> 				 
+						</ul> 
+					</li>"""
+		except:
+			return ""
 
 def current_user_name(parser, token):
-	return CurrentUserNode()
-	
+	return CurrentUserNode()	
 register.tag(current_user_name)
+
+def my_account_link(parser, token):
+	return AccountAccessNode()
+register.tag(my_account_link)
+
